b0270600125fb34e34d0ef028b6dedd307cfef96
Jam <missgud@163.com>
2018-06-06 18:57:31 +0800

    第一版

--- app/src/main/java/com/missg/mydrysister/LoaderImage.java
+++ app/src/main/java/com/missg/mydrysister/LoaderImage.java
@@ -1,13 +1,16 @@
 package com.missg.mydrysister;
 
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Handler;
 import android.os.Message;
-import android.support.annotation.Nullable;
 import android.widget.ImageView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
@@ -15,30 +18,59 @@ import java.net.URL;
  */
 
 public class LoaderImage {
-    private final int LOAD_IMAGE = 0;
+    private final int CHANGE_IMG = 0;
 
     private ImageView iv;
-    private String url;
+    private byte[] data;
 
-    public LoaderImage(ImageView iv, String url) {
+    public LoaderImage(ImageView iv) {
         this.iv = iv;
-        this.url = url;
     }
 
-    private Handler handler = new Handler(){
+    Handler handler = new Handler() {
         @Override
-        public String getMessageName(Message message) {
-            return super.getMessageName(message);
-            switch (message.what){
-                case LOAD_IMAGE :
-
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case CHANGE_IMG:
+                    if (data != null) {
+                        Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                        iv.setImageBitmap(bitmap);
+                    }
             }
-
         }
     };
 
-    private Bitmap load(){
+    public void loadImg(final String imgUrl) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
 
-        return null;
+                try {
+                    URL url = new URL(imgUrl);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.setConnectTimeout(10000);
+                    if (conn.getResponseCode() == 200) {
+                        try (InputStream in = conn.getInputStream();
+                             ByteArrayOutputStream out = new ByteArrayOutputStream();
+                        ) {
+                            byte[] bytes = new byte[1024];
+                            int len = 0;
+                            while (-1 != (len = in.read(bytes))) {
+                                out.write(bytes, 0, len);
+                            }
+                            data = out.toByteArray();
+                            handler.sendEmptyMessage(CHANGE_IMG);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
--- app/src/main/java/com/missg/mydrysister/MainActivity.java
+++ app/src/main/java/com/missg/mydrysister/MainActivity.java
@@ -2,12 +2,53 @@ package com.missg.mydrysister;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private ImageView iv;
+    private Button btn_show;
+    private List<String> urls;
+    private int pos = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv = (ImageView) findViewById(R.id.iv_show);
+        initData();
+        btn_show = (Button) findViewById(R.id.btn_show);
+        btn_show.setOnClickListener(this);
+    }
+
+    private void initData() {
+        urls = new ArrayList<>();
+        urls.add("http://ww4.sinaimg.cn/large/610dc034jw1f6ipaai7wgj20dw0kugp4.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f6gcxc1t7vj20hs0hsgo1.jpg");
+        urls.add("http://ww4.sinaimg.cn/large/610dc034jw1f6f5ktcyk0j20u011hacg.jpg");
+        urls.add("http://ww1.sinaimg.cn/large/610dc034jw1f6e1f1qmg3j20u00u0djp.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f6aipo68yvj20qo0qoaee.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f69c9e22xjj20u011hjuu.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f689lmaf7qj20u00u00v7.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/c85e4a5cjw1f671i8gt1rj20vy0vydsz.jpg");
+        urls.add("http://ww2.sinaimg.cn/large/610dc034jw1f65f0oqodoj20qo0hntc9.jpg");
+        urls.add("http://ww2.sinaimg.cn/large/c85e4a5cgw1f62hzfvzwwj20hs0qogpo.jpg");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_show :
+                if(pos > 9 ){
+                    pos = 0;
+                }
+                LoaderImage loaderImage = new LoaderImage(iv);
+                loaderImage.loadImg(urls.get(pos));
+                pos ++;
+        }
     }
 }
